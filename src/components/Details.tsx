25f35c5296eb690da713e6978d5ee026db76ac22
fabimass <massotto.fabian@gmail.com>
2022-12-03 21:42:12 -0300

    Small update to floating details

--- src/components/Details.tsx
+++ src/components/Details.tsx
@@ -7,8 +7,8 @@ const Details = (props: MatchProps) => {
     return (
         <div className={ (props.floatingDetails === true) 
                             ? (props.invertedDetails === true) 
-                                ? "right-[0px] absolute z-10 mt-[2px] h-0 w-0 group-hover:h-[300px] group-hover:w-[550px] transition-all duration-1000 ease-in-out bg-cover border-0 group-hover:border-2 border-gray-300" 
-                                : "left-[0px] absolute z-10 mt-[2px] h-0 w-0 group-hover:h-[300px] group-hover:w-[550px] transition-all duration-1000 ease-in-out bg-cover border-0 group-hover:border-2 border-gray-300"
+                                ? "right-[6px] absolute z-10 mt-[2px] h-0 w-0 group-hover:h-[300px] group-hover:w-[550px] transition-all duration-1000 ease-in-out bg-cover border-0 group-hover:border-2 border-gray-300" 
+                                : "left-[6px] absolute z-10 mt-[2px] h-0 w-0 group-hover:h-[300px] group-hover:w-[550px] transition-all duration-1000 ease-in-out bg-cover border-0 group-hover:border-2 border-gray-300"
                             : "overflow-hidden h-0 group-hover:h-[250px] group-hover:sm:h-[300px] transition-height duration-1000 ease-in-out bg-cover" 
                         } 
                         style={{ backgroundImage: `url("${props.photo}")` }}>
